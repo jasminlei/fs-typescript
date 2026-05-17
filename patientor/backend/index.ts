99e55838211a760a7a30ea35a33b20826c474433
jasminlei <jasminleinonen@hotmail.com>
2026-05-17 20:05:18 +0300

    exercise 10: enable cors for frontend

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
+
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
